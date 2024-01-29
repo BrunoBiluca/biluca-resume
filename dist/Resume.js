2018b4474d124b2875444f935333f98d390384c6
Bruno <b.b.da.costa@gmail.com>
2024-01-29 12:06:23 -0300

    🔥 Remoção dos arquivo de pacote

--- dist/Resume.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import styles from "./Resume.module.css";
-const Resume = () => /*#__PURE__*/React.createElement("div", {
-  className: styles.resume
-}, "Resume");
-export default Resume;
