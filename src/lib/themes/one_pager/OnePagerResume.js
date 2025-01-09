0e863aa76e46c92476f042a57abc11b87d38eba1
BrunoBiluca <b.b.da.costa@gmail.com>
2025-01-09 11:35:43 -0300

    ✨ Controle de exibição para Seções

--- src/lib/themes/one_pager/OnePagerResume.js
+++ src/lib/themes/one_pager/OnePagerResume.js
@@ -2,6 +2,7 @@ import Page from "../../components/page/Page"
 import GridColumns, { Main, Side } from "../../components/containers/GridColumns"
 import Section from "../../components/section/Section"
 import { cfac } from "../../core/ComponentsFactory"
+import ResumeComponent from "../../core/ResumeComponent"
 
 export default function OnePagerResume({ resume }) {
   return <Page background={resume.theme.background}>
@@ -12,18 +13,14 @@ export default function OnePagerResume({ resume }) {
         {
           resume.sections
             ?.filter(s => s.theme && s.theme["place"] === "side")
-            .map(sec =>
-              <Section key={sec.key()} section={sec} padding={0} />
-            )
+            .map(sec => new ResumeComponent(sec, <Section key={sec.key()} section={sec} padding={0} />).render())
         }
       </Side>
       <Main>
         {
           resume.sections
             ?.filter(s => !s.theme)
-            .map(sec =>
-              <Section key={sec.key()} section={sec} />
-            )
+            .map(sec => new ResumeComponent(sec, <Section key={sec.key()} section={sec} />).render())
         }
       </Main>
     </GridColumns>
