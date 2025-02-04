7bbef26ba44bc2ceb416a41f88a792a98a0d1e85
BrunoBiluca <b.b.da.costa@gmail.com>
2025-02-04 15:13:51 -0300

    🐛 Exibição correta quando o background não é passado para a página

--- src/lib/components/page/Page.js
+++ src/lib/components/page/Page.js
@@ -3,7 +3,10 @@ import styles from './Page.module.css';
 export default function Page({ background, children }) {
   return (
     <div className={styles.page}>
-      <img src={background} className={styles.background} alt="page background" />
+      {
+        background &&
+        <img src={background} className={styles.background} alt="page background" />
+      }
       <div className={styles.content}>
         {children}
       </div>
